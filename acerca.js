0f05a30f476e9080042be86dee1f6e159c95c625
guaranitour <merelesrichard@gmail.com>
2026-06-16 12:05:19 -0300

    Delete acerca.js

--- acerca.js
+++
@@ -1,8 +0,0 @@
-// Versión del sistema (simple pero escalable)
-const APP_VERSION = "1.0.0";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const versionElement = document.getElementById("version");
-  versionElement.textContent = "Versión " + APP_VERSION;
-});
-``
