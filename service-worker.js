840c741e3ab41cf7c10e4a71f8dcf0b0cb95df7f
guaranitour <merelesrichard@gmail.com>
2026-06-30 15:55:16 -0300

    Update CACHE_NAME to version 35

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME    = 'guarani-tour-v34';
+const CACHE_NAME    = 'guarani-tour-v35';
 const CACHE_IMAGES  = 'guarani-tour-images-v1';
 const CACHE_EXTERN  = 'guarani-tour-extern-v1';
 
