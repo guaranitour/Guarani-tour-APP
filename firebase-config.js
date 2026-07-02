90e51426af84fa3da7ae61b7d58852af21d99c8b
guaranitour <merelesrichard@gmail.com>
2026-07-02 14:36:14 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrdh7b4Mkud0_FsCsn9OcPeiJoTjJAE1c",
+  authDomain: "guarani-tour.firebaseapp.com",
+  projectId: "guarani-tour",
+  storageBucket: "guarani-tour.firebasestorage.app",
+  messagingSenderId: "715907999724",
+  appId: "1:715907999724:web:292ffc9c1cc5baa56d82c9"
+};
+
+const FIREBASE_VAPID_KEY =
+  "BB_Wmw6k5YTg3oPHOqx_SqnmEGieAkiCgOYfXrYtjDsvYxTZz7hBrB78yYn2LQmzw3tzUFrpZsMZAtEvWoJhZtk";
