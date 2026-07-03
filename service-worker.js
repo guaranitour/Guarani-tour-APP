33c3d3a88cc7da6ee1e1e6d9cf556bb20ce63f57
guaranitour <merelesrichard@gmail.com>
2026-07-02 22:58:00 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME    = 'guarani-tour-v45';
+const CACHE_NAME    = 'guarani-tour-v46';
 const CACHE_IMAGES  = 'guarani-tour-images-v1';
 const CACHE_EXTERN  = 'guarani-tour-extern-v1';
 
