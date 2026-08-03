da91d07c9b09e436a16fb2c751b2599ab568a67e
guaranitour <merelesrichard@gmail.com>
2026-08-03 07:26:22 -0300

    Update cache name to guarani-tour-v76

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME    = 'guarani-tour-v75';
+const CACHE_NAME    = 'guarani-tour-v76';
 const CACHE_IMAGES  = 'guarani-tour-images-v1';
 const CACHE_EXTERN  = 'guarani-tour-extern-v1';
 
